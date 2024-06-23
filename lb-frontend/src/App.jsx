20a9a295ccc10060b086725c5217391227f9503e
REZ-OAN <ahmedabir.rez@gmail.com>
2024-06-23 16:24:29 +0600

    created structure of inputs and the ui page

---
+++ lb-frontend/src/App.jsx
@@ -0,0 +1,63 @@
+import React, { Fragment, useEffect, useState } from 'react'
+
+const App = () => {
+  const [subnet,setSubnet] = useState('');
+  const [clientIp,setClientIp] = useState('');
+  const [lbIp,setLbIp] = useState('');
+  const [serverIp,setServerIp] = useState('');
+  const handleSubnet = async (e) =>{
+    e.prevent.Default();
+  };
+  const handleClientIp = async (e)=>{
+    e.prevent.Default();
+  };
+  const handleLbIp = async (e) => {
+    e.prevent.Default();
+  };
+
+  const handleServerIp = async (e) => {
+    e.prevent.Default();
+  };
+
+  useEffect(()=>{
+    const fetchInfo = async ()=> {
+      console.log("fetching_data...");
+    };
+    fetchInfo();
+  },[])
+  return (
+    <Fragment>
+      <div className='container'>
+          <div className='left-panel'>
+            <div className='subnet-input'>
+              <label>SUBNET </label>
+              <input type="text" value={subnet} onChange={(e)=>{setSubnet(e.target.value)}}></input>
+              <button onClick={(e)=> handleSubnet(e)}>Create Subnet</button>
+            </div>
+            <div className='clientip-input'>
+              <label>CLIENT IP </label>
+              <input type="text" value={clientIp} onChange={(e)=>{setClientIp(e.target.value)}}></input>
+              <button onClick={(e)=> handleClientIp(e)}>Launch Client</button>
+            </div>
+            <div className='lb-input'>
+              <label>LOAD-BALANCER IP </label>
+              <input type="text" value={lbIp} onChange={(e)=>{setLbIp(e.target.value)}}></input>
+              <button onClick={(e)=> handleLbIp(e)}>Launch LoadBalancer</button>
+            </div>
+            <div className='server-input'>
+              <label>SERVER IP </label>
+              <input type="text" value={serverIp} onChange={(e)=>{setServerIp(e.target.value)}}></input>
+              <button onClick={(e)=> handleLbIp(e)}>Launch Server</button>
+            </div>
+          </div>
+          <div className='right-panel'>
+            {
+            //TODO Table  key, ip-address, mac-address
+            }
+          </div>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
